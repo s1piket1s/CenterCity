416f0b0fdafa2d78a54cdaff7ef075a668d0184d
S1piket1S <klimatwua@gmail.com>
2020-02-02 15:47:58 +0200

    Add starts sheets API

--- src/main/java/com/cetnetcity/server/controller/DataController.java
+++ src/main/java/com/cetnetcity/server/controller/DataController.java
@@ -6,13 +6,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
 @Controller
 @RequestMapping("/data")
 public class DataController {
 
     @RequestMapping(value = "/get", method = RequestMethod.GET)
     @ResponseBody
-    public String getData(ModelMap model){
-        return "My data.";
+    public String getData(ModelMap model) throws IOException, GeneralSecurityException {
+        return com.cetnetcity.server.gsheet.SheetsGoogle.Method();
     }
 }
