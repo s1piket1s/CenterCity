7aa9f785b0fe20beb1444d1360515074df65cefd
S1piket1S <klimatwua@gmail.com>
2020-02-02 22:25:16 +0200

    Add JSP and servlet

---
+++ src/main/java/com/cetnetcity/server/servlets/MyServlet.java
@@ -0,0 +1,33 @@
+package com.cetnetcity.server.servlets;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/server")
+public class MyServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        super.doPost(request, response);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+
+        String varTextA = "Hello World!";
+        request.setAttribute("textA", varTextA);
+        String varTextB = "It JSP.";
+        request.setAttribute("textB", varTextB);
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/index.jsp");
+        dispatcher.forward(request, response);
+    }
+}
